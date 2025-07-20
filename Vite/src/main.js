d1aec6aafa5f8f9ade0c5fc7a8c1913e76fe9d1d
raykaris <ryanmuthoni050@gmail.com>
2025-07-20 17:05:02 +0300

    initial commit

---
+++ Vite/src/main.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const canvas = document.getElementById('canvas');
+
+// creating the scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
+
+// adding the camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// adding the object
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshBasicMaterial({ color: '#468585' });
+const dodecahedron = new THREE.Mesh(geometry, material);
+
+const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2); 
+const boxMaterial = new THREE.MeshBasicMaterial({ color: '#B4B4B3' });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.y = -1.5;
+
+scene.add(dodecahedron);
+scene.add(box);
+
+// Adding lighting
+const light = new THREE.SpotLight(0x006769, 100);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+// Setting up the renderer
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+// adding orbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+// Adding Animation
+function animate() {
+    requestAnimationFrame(animate);
+
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y =+ 0.01;
+
+    box.rotation.y += 0.005;
+
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+animate();
